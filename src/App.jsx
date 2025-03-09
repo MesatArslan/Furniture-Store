fe6225084a8e28c6bf5e5d9ca4cebc4bb76fa5c4
Mahmut Esat Arslan <142013436+MesatArslan@users.noreply.github.com>
2025-03-09 21:02:10 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './assets/components/Navbar';
 import Home from './assets/pages/Home';
 import About from './assets/pages/About';
